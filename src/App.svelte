4b4a50d9059604b7dc78a5db4ec5cd7923811d2e
TenthYoung <tenthyoung@gmail.com>
2019-12-16 17:36:11 -0800

    start

---
+++ src/App.svelte
@@ -0,0 +1,36 @@
+<script>
+	export let name;
+	let clickCount = 0;
+
+	function handleClick() {
+		clickCount++;
+	}
+</script>
+
+<main>
+	<h1>Clicki</h1>
+	<p>Click count: {clickCount}</p>
+	<button on:click="{() => handleClick()}">Click me</button>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
